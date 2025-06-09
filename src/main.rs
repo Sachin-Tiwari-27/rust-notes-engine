c829550dd56a153b345d94ea2cd1878a921bc8ea
Sachin-Tiwari-27 <125535995+Sachin-Tiwari-27@users.noreply.github.com>
2025-06-09 15:15:01 +0530

    CLI interface added using clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,97 @@
 mod notes;
+use clap::{Parser, Subcommand};
 use notes::{Note, Tag, print_note};
 
+#[derive(clap::Parser)]
+#[command(name = "Rust Notes Engine")]
+#[command(about = "A CLI note manager written in Rust", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(clap::Subcommand)]
+enum Commands{
+    ///Add new Note
+    Add{
+        title: String,
+        body: String,
+        tag: String,
+    },
+
+    ///List all notes
+    List,
+
+    ///Filters all notes by tag
+    Filter{
+        tag: String,
+    },
+
+}
 fn main() {
-    let note = Note{
-        title: "Rust Study".into(),
-        body: "Focus on error handling today".into(),
-        tag: Tag::Work,
-    };
-    print_note(&note);
+
+    let cli = Cli::parse();
+
+    let mut notes: Vec<Note> = Vec::new();
+
+    match &cli.command {
+        Commands::Add { title, body, tag } => {
+            let parsed_tag = match tag.to_lowercase().as_str() {
+                "work" => Tag::Work,
+                "personal" => Tag::Personal,
+                "urgent" => Tag::Urgent,
+                _=> {
+                    println!("Invalid Tag, use Work, Personal, or Urgent.");
+                    return;
+                }
+            };
+
+            let note = Note {
+                title: title.clone(),
+                body: body.clone(),
+                tag: parsed_tag,
+            };
+
+            notes.push(note);
+            println!("✅ Note added.")
+        }
+        Commands::List => {
+            if notes.is_empty(){
+                println!("No notes yet.");
+            } else {
+                for note in &notes {
+                    print_note(note);
+                }
+            }
+
+        }
+
+        Commands::Filter {tag} => {
+            let parsed_tag = match tag.to_lowercase().as_str() {
+                "work" => Tag::Work,
+                "personal" => Tag::Personal,
+                "urgent" => Tag::Urgent,
+                _=> {
+                    println!{"Invalid tag. Use Work, Personal or Urgent."};
+                    return;
+                }
+            };
+
+            let filtered : Vec<&Note> = notes
+            .iter()
+            .filter(|n| n.tag == parsed_tag)
+            .collect();
+            
+            if filtered.is_empty(){
+                println!("No Notes found for tag {:?}", parsed_tag);
+            } else {
+                for note in filtered {
+                    print_note(note);
+                }
+            }
+        }
+
+    }
+    
 
 }
